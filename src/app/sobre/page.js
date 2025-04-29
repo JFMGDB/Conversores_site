e3ad09640d656ce99e180f48faf6b8fdaa71d516
Felipe <fguerra127@gmail.com>
2025-04-28 23:33:02 -0300

    ajustes

---
+++ src/app/sobre/page.js
@@ -0,0 +1,10 @@
+export default function Sobre() {
+    return (
+        <>
+            <h1 className="text-2xl font-bold mb-4">Sobre o Desenvolvedor</h1>
+            <p>
+                Meu nome é José Felipe e atualmente sou estudante de desenvolvimento front-end, interessado por criar interfaces elegantes e funcionais. Embora eu ainda não tenha experiência profissional na área, tenho me dedicado aos meus estudos de React, Next.js e Tailwind CSS
+            </p>
+        </>
+    )
+}
